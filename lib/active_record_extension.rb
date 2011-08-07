e7d3c53e53605596e17425b8f8d03814f39d462e
Ryan Berdeen <ryan@ryanberdeen.com>
2011-08-06 23:00:00 -0400

    fix respond_to?

--- lib/active_record_extension.rb
+++ lib/active_record_extension.rb
@@ -9,14 +9,14 @@ module RailsMoney
         money = args.first.kind_of?(Money) ? args.first : Money.new(args.first)
         write_attribute(method_name,money.cents) 
       else
-        Money.create_from_cents(read_attribute(method_name))      
+        Money.create_from_cents(read_attribute(method_name))
       end
     else 
       super
     end
   end
 
-  def respond_to?(method)
+  def respond_to?(method, include_private=false)
     method_name = method.to_s.chomp("=")
 
     @attributes.include?("#{method_name}_in_cents") || super
